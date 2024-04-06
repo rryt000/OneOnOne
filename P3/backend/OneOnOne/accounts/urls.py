2299b5f4e8de09b10d1bb303b347125d4eabbe48
Rubin Rastogi <113395998+rryt000@users.noreply.github.com>
2024-04-06 19:09:43 -0400

    readding changes from robinrswanson

--- P3/backend/OneOnOne/accounts/urls.py
+++ P3/backend/OneOnOne/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import RegisterView, ProfileView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import MyTokenObtainPairView
+
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name="sign_up"),
     path('', ProfileView.as_view(), name="profile"),
