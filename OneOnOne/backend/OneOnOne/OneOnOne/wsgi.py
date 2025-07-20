11638791656cfc4bb85a0aaa08638ad1d67d024e
rryt000 <rastogirubin@gmail.com>
2025-07-21 04:44:39 +0530

    updated wsgi.py

--- OneOnOne/backend/OneOnOne/OneOnOne/wsgi.py
+++ OneOnOne/backend/OneOnOne/OneOnOne/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'OneOnOne'))
+sys.path.append(os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OneOnOne.settings')
 
