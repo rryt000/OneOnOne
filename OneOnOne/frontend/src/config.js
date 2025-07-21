82efd031d870a309e58a3f88273e974a0ad6a276
rryt000 <rastogirubin@gmail.com>
2025-07-22 02:12:42 +0530

    edit config.js

--- OneOnOne/frontend/src/config.js
+++ OneOnOne/frontend/src/config.js
@@ -1,9 +1,3 @@
-export const backendUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://oneonone-ht0z.onrender.com"
-    : "http://localhost:8000";
+export const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-export const frontendUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://oneonone-project.netlify.app"
-    : "http://localhost:3000";
+export const frontendUrl = process.env.REACT_APP_FRONTEND_URL;
