28c990ffd8831306366bfeb5743c5a41b69a58d5
rryt000 <rastogirubin@gmail.com>
2025-07-21 17:23:09 +0530

    changing db to postgresql

--- OneOnOne/backend/OneOnOne/settings.py
+++ OneOnOne/backend/OneOnOne/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -103,10 +105,9 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.UserData'
